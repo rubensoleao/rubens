12d4fe5b50b8716e7e1f6b324e4c2eb46e42aec2
rubensoleao <rubensoleao@gmail.com>
2024-07-02 19:05:09 -0400

    feat: MemoryForm date validation

--- src/app/forms/memory-form.tsx
+++ src/app/forms/memory-form.tsx
@@ -1,8 +1,9 @@
 import { Button, Field, Input, Label, Textarea } from '@headlessui/react'
 import clsx from 'clsx'
-import React from 'react'
+import React, { useState } from 'react'
 import InputMask from 'react-input-mask'
 import { Memory } from './../routes/root'
+import { parse, isValid } from 'date-fns'
 
 interface MemoryFormProps {
   defaultValue?: Memory
@@ -10,29 +11,48 @@ interface MemoryFormProps {
 }
 
 const MemoryForm: React.FC<MemoryFormProps> = ({ defaultValue, onSubmit }) => {
+  const validateDate = (dateString: string) =>
+    isValid(parse(dateString, 'MM/dd/yyyy', new Date()))
+  const [formError, setFormError] = useState<string | undefined>(undefined)
+
   const submitData = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
     const formData = new FormData(e.currentTarget)
     const title = formData.get('title') as string
     const description = formData.get('description') as string
+    const date = formData.get('date') as string
+    const nDate = parse(date, 'MM/dd/yyyy', new Date())
+    console.log('VALIDATIRON', validateDate(date), date, nDate, isValid(nDate))
+    if (!validateDate(date)) {
+      setFormError('Invalid Date')
+      return
+    }
 
     if (onSubmit) {
       onSubmit()
     }
-
   }
 
   return (
     <div className='flex flex-col space-y-2'>
       <form onSubmit={submitData} className='mt-4'>
         <Field as='div'>
-          <Label className='text-sm/6 font-medium'>Date</Label>
+          <Label className='text-sm/6 font-medium'>
+            Date{' '}
+            <span className=' text-[12px] text-gray-400 pl-1'>
+              ( MM/DD/YYYY )
+            </span>
+            <span />
+          </Label>
           <InputMask
             mask='99/99/9999'
             defaultValue={'22/05/1992'}
             name='date'
-            className='block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+            onChange={ ()=> {if (formError) { setFormError(undefined)} }}// reset form error 
+            className={clsx(
+              { 'border-red-400 focus:border-red-600 border-x-8': formError },
+              'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+            )}
             placeholder='MM/DD/YYYY'
             default='05/22/1992'
           />
