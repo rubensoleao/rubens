8ce109a3034fb3df13607ee002687ae7e75c1a5f
rubensoleao <rubensoleao@gmail.com>
2024-07-02 10:15:09 -0400

    feat: add dropdown menu component

--- src/app/routes/root.tsx
+++ src/app/routes/root.tsx
@@ -9,7 +9,7 @@ import {
 } from '@headlessui/react'
 import { Fragment, useState, useEffect } from 'react'
 import './root.css'
-
+import  DropdownMenu  from './../components/dropdown-menu'
 interface Memory {
   id: number
   title: string
@@ -121,8 +121,8 @@ export default function Root() {
           <h1 className='text-2xl font-semibold text-gray-900'>
             Jae's memory lane
           </h1>
-          <Button className='inline-flex  justify-center rounded-md border border-gray-300 shadow-sm p-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500' >
-            <ShareIcon className='w-4 h-4 text-blue-600'/>
+          <Button className='inline-flex  justify-center rounded-md border border-gray-300 shadow-sm p-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'>
+            <ShareIcon className='w-4 h-4 text-blue-600' />
           </Button>
         </div>
         <div>
@@ -134,60 +134,12 @@ export default function Root() {
             story. Embracing challenges and cherishing relationships, Jae
             continues to craft a unique and inspiring life history.
           </p>
-
         </div>
         <div className='flex justify-between items-center mb-4'>
           <Button className='inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'>
             + New memory
           </Button>
-
-          <Menu as='div' className='relative inline-block text-left'>
-            <MenuButton className='inline-flex  items-center justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'>
-              Sort
-              <ChevronDownIcon className='h-4 w-4 size-4 fill-black/60' />
-            </MenuButton>
-            <Transition
-              as={Fragment}
-              enter='transition ease-out duration-100'
-              enterFrom='transform opacity-0 scale-95'
-              enterTo='transform opacity-100 scale-100'
-              leave='transition ease-in duration-75'
-              leaveFrom='transform opacity-100 scale-100'
-              leaveTo='transform opacity-0 scale-95'
-            >
-              <MenuItems
-                className='origin-top-left absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
-                anchor='bottom end'
-              >
-                <div className='py-1'>
-                  <MenuItem>
-                    {({ focus }) => (
-                      <a
-                        href='#'
-                        className={`${
-                          focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                        } block px-4 py-2 text-sm`}
-                      >
-                        Older to new
-                      </a>
-                    )}
-                  </MenuItem>
-                  <MenuItem>
-                    {({ focus }) => (
-                      <a
-                        href='#'
-                        className={`${
-                          focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                        } block px-4 py-2 text-sm`}
-                      >
-                        New to older
-                      </a>
-                    )}
-                  </MenuItem>
-                </div>
-              </MenuItems>
-            </Transition>
-          </Menu>
+          <DropdownMenu options={['Older to new', 'New to older']} />
         </div>
         <div>
           {db.memories.map((memory) => (
