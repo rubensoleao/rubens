bca3b566c5c972941dfdca5833c5b4f10bd48950
rubensoleao <rubensoleao@gmail.com>
2024-07-04 18:49:58 -0400

    fix: errors

--- src/app/components/file-upload-input.tsx
+++ src/app/components/file-upload-input.tsx
@@ -11,7 +11,7 @@ const FileUploadInput: React.FC<FileUploadInputProps> = ({
   className,
   onFileChange,
 }) => {
-  const [file, setFile] = useState<File | undefined>(undefined)
+  const [, setFile] = useState<File | undefined>(undefined)
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target || !e.target.files) {
